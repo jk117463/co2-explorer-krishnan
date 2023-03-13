95998d1bae74197cc85d73915c453e523c84282a
jk117463 <92452727+jk117463@users.noreply.github.com>
2023-03-13 10:21:02 +0100

    Add files via upload

---
+++ co2_explorer.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 13 09:50:47 2023
+
+@author: jathi
+"""
+
+import pandas as pd
+import plotly.express as px
+from pandas_datareader import wb
+
+#from jupyter_dash import JupyterDash
+from dash import dcc, html, Dash
+from dash.dependencies import Input, Output
+
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
+
+dbc_css = 'https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.2/dbc.min.css'
+load_figure_template('bootstrap')
+
+app = Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP, dbc_css])
+server = app.server #Tell the online server how to deploy the app to online server
+
+app.layout = dbc.Container(
+    children = [
+        
+        # Header
+        html.H1('CO2 emissions around the world'),
+        dcc.Markdown(
+            """Data on emissions and potential drivers are extracted from the 
+               [World Development Indicators](https://datatopics.worldbank.org/world-development-indicators/) 
+               database."""
+        ),
+       html.P('This is a new paragraph')
+        
+    ],
+    className = 'dbc'
+)
+
+if __name__ == '__main__':
+    #app.run_server()
+    app.run_server(debug = True)
